f4da6dfad647ddbf8a5737a5b1bc447a7d59c975
Lê Quang Minh <lqminh2207@gmail.com>
2022-12-15 21:51:25 +0700

    init

--- src/routes/admin.ts
+++ src/routes/admin.ts
@@ -4,7 +4,6 @@ import { Router } from "express"
 import AdminController from '../app/controllers/AdminController';
 const router = Router()
 
-router.post('/register', AdminController.register)
 router.get('/listAll/:projectId([0-9]+)/projects/:taskId([0-9]+)/tasks', [checkJwt, checkRole(["ADMIN"])], AdminController.getTask)
 router.get('/listAll/:projectId([0-9]+)/project', [checkJwt, checkRole(["ADMIN"])], AdminController.getProject)
 router.patch('/listAll/:id([0-9]+)', [checkJwt, checkRole(["ADMIN"])], AdminController.updateUser)
